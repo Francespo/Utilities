2608dc835681261f3314089db07c0e39a3bcd85f
3spo14 <francesco7.esposito@gmail.com>
2024-11-30 17:08:53 +0100

    Update FloatExtensions.cs

--- Runtime/Extensions/ExtensionClasses/FloatExtensions.cs
+++ Runtime/Extensions/ExtensionClasses/FloatExtensions.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 namespace Francespo.Utilities.Extensions
 {
@@ -6,5 +7,9 @@ namespace Francespo.Utilities.Extensions
     {
         public static bool IsApproximately(this float value, float other) => Mathf.Approximately(value, other);
         public static bool IsApproximatelyZero(this float value) => value.IsApproximately(0);
+
+        public static int Sign(this float value) => Math.Sign(value);
+        public static float USignF(this float value) => Mathf.Sign(value);
+        public static int USign(this float value) => (int)Mathf.Sign(value);
     }
 }
