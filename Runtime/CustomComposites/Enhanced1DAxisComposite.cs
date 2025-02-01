c51151fc081378c2bcae5eaceedb7a3313e5bdcb
Francesco Esposito <francesco7.esposito@gmail.com>
2025-02-01 18:13:54 +0100

    Update Enhanced1DAxisComposite.cs

--- Runtime/CustomComposites/Enhanced1DAxisComposite.cs
+++ Runtime/CustomComposites/Enhanced1DAxisComposite.cs
@@ -7,30 +7,118 @@ using UnityEngine.InputSystem.Utilities;
 using UnityEngine.Scripting;
 
 #if UNITY_EDITOR
-[UnityEditor.InitializeOnLoad]
+[InitializeOnLoad] // Automatically register in editor.
 #endif
-[Preserve]
-[DisplayStringFormat("{left}/{right}")]
-[DisplayName("Left/Right Composite")]
-public class Enhanced1DAxisComposite : InputBindingComposite<float>
-{
-    /// <summary>
-    /// Binding for the button represents the left (that is, <c>(-1,0)</c>) direction of the vector.
-    /// </summary>
-    /// <remarks>
-    /// This property is automatically assigned by the input system.
-    /// </remarks>
-    // ReSharper disable once MemberCanBePrivate.Global
-    // ReSharper disable once FieldCanBeMadeReadOnly.Global
-    [InputControl(layout = "Axis")] public int left = 0;
 
     /// <summary>
-    /// Binding for the button that represents the right (that is, <c>(1,0)</c>) direction of the vector.
+    /// A single axis value computed from one axis that pulls in the <see cref="negative"/> direction (<see cref="minValue"/>) and one
+    /// axis that pulls in the <see cref="positive"/> direction (<see cref="maxValue"/>).
     /// </summary>
     /// <remarks>
-    /// This property is automatically assigned by the input system.
+    /// The limits of the axis are determined by <see cref="minValue"/> and <see cref="maxValue"/>.
+    /// By default, they are set to <c>[-1..1]</c>. The values can be set as parameters.
+    ///
+    /// <example>
+    /// <code>
+    /// var action = new InputAction();
+    /// action.AddCompositeBinding("Axis(minValue=0,maxValue=2)")
+    ///     .With("Negative", "&lt;Keyboard&gt;/a")
+    ///     .With("Positive", "&lt;Keyboard&gt;/d");
+    /// </code>
+    /// </example>
+    ///
+    /// If both axes are actuated at the same time, the behavior depends on <see cref="whichSideWins"/>.
+    /// By default, neither side will win (<see cref="WhichSideWins.Neither"/>) and the result
+    /// will be 0 (or, more precisely, the midpoint between <see cref="minValue"/> and <see cref="maxValue"/>).
+    /// This can be customized to make the positive side win (<see cref="WhichSideWins.Positive"/>)
+    /// or the negative one (<see cref="WhichSideWins.Negative"/>).
+    ///
+    /// This is useful, for example, in a driving game where break should cancel out accelerate.
+    /// By binding <see cref="negative"/> to the break control(s) and <see cref="positive"/> to the
+    /// acceleration control(s), and setting <see cref="whichSideWins"/> to <see cref="WhichSideWins.Negative"/>,
+    /// if the break button is pressed, it will always cause the acceleration button to be ignored.
+    ///
+    /// The actual <em>absolute</em> values of <see cref="negative"/> and <see cref="positive"/> are used
+    /// to scale <see cref="minValue"/> and <see cref="maxValue"/> respectively. So if, for example, <see cref="positive"/>
+    /// is bound to <see cref="Gamepad.rightTrigger"/> and the trigger is at a value of 0.5, then the resulting
+    /// value is <c>maxValue * 0.5</c> (the actual formula is <c>midPoint + (maxValue - midPoint) * positive</c>).
     /// </remarks>
-    [InputControl(layout = "Axis")] public int right = 0;
+    [DisplayStringFormat("{negative}/{positive}")]
+public class Enhanced1DAxisComposite : InputBindingComposite<float>
+{
+/// <summary>
+        /// Binding for the axis input that controls the negative [<see cref="minValue"/>..0] direction of the
+        /// combined axis.
+        /// </summary>
+        /// <remarks>
+        /// This property is automatically assigned by the input system.
+        /// </remarks>
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once FieldCanBeMadeReadOnly.Global
+        [InputControl(layout = "Axis")] public int negative = 0;
+
+        /// <summary>
+        /// Binding for the axis input that controls the positive [0..<see cref="maxValue"/>] direction of the
+        /// combined axis.
+        /// </summary>
+        /// <remarks>
+        /// This property is automatically assigned by the input system.
+        /// </remarks>
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once FieldCanBeMadeReadOnly.Global
+        [InputControl(layout = "Axis")] public int positive = 0;
+
+        /// <summary>
+        /// The lower bound that the axis is limited to. -1 by default.
+        /// </summary>
+        /// <remarks>
+        /// This value corresponds to the full actuation of the control(s) bound to <see cref="negative"/>.
+        ///
+        /// <example>
+        /// <code>
+        /// var action = new InputAction();
+        /// action.AddCompositeBinding("Axis(minValue=0,maxValue=2)")
+        ///     .With("Negative", "&lt;Keyboard&gt;/a")
+        ///     .With("Positive", "&lt;Keyboard&gt;/d");
+        /// </code>
+        /// </example>
+        /// </remarks>
+        /// <seealso cref="maxValue"/>
+        /// <seealso cref="negative"/>
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once FieldCanBeMadeReadOnly.Global
+        [Tooltip("Value to return when the negative side is fully actuated.")]
+        public float minValue = -1;
+
+        /// <summary>
+        /// The upper bound that the axis is limited to. 1 by default.
+        /// </summary>
+        /// <remarks>
+        /// This value corresponds to the full actuation of the control(s) bound to <see cref="positive"/>.
+        ///
+        /// <example>
+        /// <code>
+        /// var action = new InputAction();
+        /// action.AddCompositeBinding("Axis(minValue=0,maxValue=2)")
+        ///     .With("Negative", "&lt;Keyboard&gt;/a")
+        ///     .With("Positive", "&lt;Keyboard&gt;/d");
+        /// </code>
+        /// </example>
+        /// </remarks>
+        /// <seealso cref="minValue"/>
+        /// <seealso cref="positive"/>
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once FieldCanBeMadeReadOnly.Global
+        [Tooltip("Value to return when the positive side is fully actuated.")]
+        public float maxValue = 1;
+        
+        /// <summary>
+        /// The value that is returned if the composite is in a neutral position, that is, if
+        /// neither <see cref="positive"/> nor <see cref="negative"/> are actuated or if
+        /// <see cref="whichSideWins"/> is set to <see cref="WhichSideWins.Neither"/> and
+        /// both <see cref="positive"/> and <see cref="negative"/> are actuated.
+        /// </summary>
+        public float midPoint => (maxValue + minValue) / 2;
 
     [Tooltip("If both the positive and negative side are actuated, decides what value to return. 'Neither' (default) means that " +
     "the resulting value is 0. 'Positive' means that 1 will be returned. 'Negative' means that " +
@@ -76,23 +164,40 @@ public class Enhanced1DAxisComposite : InputBindingComposite<float>
             }
         
         if (leftIsPressed)
-            return -1f;
+            return minValue;
         else if (rightIsPressed)
-            return 1f;
+            return maxValue;
         else
-            return 0f;
+            return midPoint;
     }
+    
+    /// <inheritdoc />
+    public override float EvaluateMagnitude(ref InputBindingCompositeContext context)
+    {
+        var value = ReadValue(ref context);
+        if (value < midPoint)
+        {
+            value = Mathf.Abs(value - midPoint);
+            return NormalizeProcessor.Normalize(value, 0, Mathf.Abs(minValue), 0);
+        }
 
-#if UNITY_EDITOR
-    static Enhanced1DAxisComposite() => Initialize();
-#endif
-
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-    static void Initialize()
+        value = Mathf.Abs(value - midPoint);
+        return NormalizeProcessor.Normalize(value, 0, Mathf.Abs(maxValue), 0);
+    }
+    
+    static Enhanced1DAxisComposite()
     {
         InputSystem.RegisterBindingComposite<Enhanced1DAxisComposite>();
     }
+    
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Init() {} // Trigger static constructor.
 
+
+    /// <summary>
+    /// What happens to the value of an <see cref="AxisComposite"/> if both <see cref="positive"/>
+    /// and <see cref="negative"/> are actuated at the same time.
+    /// </summary>
     public enum WhichSideWins
     {
         Positive,
